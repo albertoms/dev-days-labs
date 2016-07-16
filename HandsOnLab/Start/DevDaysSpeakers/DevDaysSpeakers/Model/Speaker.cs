6326fc65256f09d7bbf3f6830632094287a2cea6
Luis Guerrero <albertoms@econduce.mx>
2016-07-16 15:21:43 -0500

    terminar modelo y viewmodel

--- HandsOnLab/Start/DevDaysSpeakers/DevDaysSpeakers/Model/Speaker.cs
+++ HandsOnLab/Start/DevDaysSpeakers/DevDaysSpeakers/Model/Speaker.cs
@@ -8,6 +8,35 @@ namespace DevDaysSpeakers.Model
 {
     public class Speaker : AppServiceHelpers.Models.EntityData
     {
-        
+		public string Id {
+			get;
+			set;
+		}
+
+		public string Name {
+			get;
+			set;
+		}
+
+		public string Description {
+			get;
+			set;
+		}
+
+		public string Website {
+			get;
+			set;
+		}
+
+		public string Title {
+			get;
+			set;
+		}
+
+		public string Avatar {
+			get;
+			set;
+		}
+
     }
 }
--- HandsOnLab/Start/DevDaysSpeakers/DevDaysSpeakers/ViewModel/SpeakersViewModel.cs
+++ HandsOnLab/Start/DevDaysSpeakers/DevDaysSpeakers/ViewModel/SpeakersViewModel.cs
@@ -28,5 +28,67 @@ namespace DevDaysSpeakers.ViewModel
 
 			changed.Invoke (this, new PropertyChangedEventArgs(name));
 		}
+
+		bool busy;
+
+		public bool IsBusy
+		{
+			get { return busy; }
+			set
+			{
+				busy = value;
+				OnPropertyChanged ();
+
+				GetSpeakersCommand.ChangeCanExecute ();
+			}
+		}
+
+		public ObservableCollection<Speaker> Speakers { get; set; }
+
+		public Command GetSpeakersCommand { get; set; }
+
+
+		public SpeakersViewModel()
+		{
+			Speakers = new ObservableCollection<Speaker> ();
+
+			GetSpeakersCommand = new Command(
+				async () => await GetSpeakers (),
+				() => !IsBusy);
+		}
+
+		async Task GetSpeakers()
+		{
+			if (IsBusy)
+				return;
+
+			Exception error = null;
+
+			try {
+				IsBusy = true;
+
+				using (var client  = new HttpClient())
+				{
+					var json = await client.GetStringAsync ("http://demo4404797.mockable.io/speakers");
+
+					var items = JsonConvert.DeserializeObject<List<Speaker>> (json);
+
+					Speakers.Clear ();
+
+					foreach (var item in items) {
+						Speakers.Add (item);
+					}
+				}
+			}
+			catch (Exception ex) {
+				error = ex;
+			}
+			finally {
+				IsBusy = false;
+			}
+
+			if (error != null)
+				await Application.Current.MainPage.DisplayAlert ("Error!", error.Message, "OK");
+		}
 	}
 }
